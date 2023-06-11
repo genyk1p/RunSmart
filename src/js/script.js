017bb76b953f9787030045f12496262bcceb466d
Evhen <geny.k1p@gmail.com>
2023-06-11 11:46:33 +0300

    move slider to module

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,6 @@
 "use strict";
-import { tns } from "tiny-slider/src/tiny-slider";
+import slider from "./modules/slider";
 
 document.addEventListener("DOMContentLoaded", () =>{
-    const left = document.querySelector(".slider__left"),
-          right = document.querySelector(".slider__right");
-
-    const hidArow = () => {
-      if (document.documentElement.clientWidth <= 768){
-        left.style.display = "none";
-        right.style.display = "none";
-      } else {
-        left.style.display = "block";
-        right.style.display = "block";
-      }
-    }
-    hidArow();
-
-    let slider = tns({
-        container: '.slider__inner',
-        items: 1,
-        slideBy: 'page',
-        controls: false,
-        nav: false,
-        mouseDrag: true,
-        speed: 1000,
-      });
-    left.addEventListener("click", () =>{
-      slider.goTo("prev")
-    })
-    right.addEventListener("click", () =>{
-      slider.goTo("next")
-    })
-    window.addEventListener('resize', function() {
-      hidArow();
-      slider.refresh(); // Обновить слайдер
-    });
-    setInterval(()=>{
-      slider.goTo("next")
-    }, 10000)
+  slider();
 });
