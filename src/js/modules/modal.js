efba0ea1a5b6ea6b6951f7645da120a964907be4
Evhen <geny.k1p@gmail.com>
2023-06-15 00:00:50 +0300

    added mask to phone input

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,9 +1,18 @@
-
+import { Validator, enLang as en } from '@upjs/facile-validator';
+import IMask from 'imask';
 function modal (){
     const callBackBtn = document.querySelectorAll(".button[data-btn='call-back']"),
           thanksModalBtn = document.querySelector(".button[data-btn='thanks-modal']"),
           modalCallBackBtn = document.querySelector(".button[data-btn='modal-call-back-btn']");
     let modalThanksTimeout;
+
+    // Создаем маску на инпут телефонного номераgit
+    const phoneInputs = document.querySelectorAll('.form__phone');
+    phoneInputs.forEach(input =>{
+        IMask(input, {
+            mask: '+{420} 000 000 000',
+            });
+    })
 
     // Отображает модальное окно
     function showModal(modal) {
@@ -16,9 +25,8 @@ function modal (){
         modal.querySelector('.modal__item-wrapper').classList.remove("modal__item-wrapper_show")
     }
     // открывает модальное окно с благодарностью
-    function showThanksModal(e) {
+    function showThanksModal() {
         const modal = document.querySelector("#modal-thanks");
-        e.preventDefault();
         showModal(modal);
         modalThanksTimeout = setTimeout(()=>{
             modalHide(modal);
@@ -32,41 +40,74 @@ function modal (){
  
         })
     })
-    document.querySelector('#modal-call-back').querySelector('.modal__cross').addEventListener('click', (e)=>{
+    document.querySelector('#modal-call-back .modal__cross').addEventListener('click', (e)=>{
         const modal = e.target.parentElement.parentElement.parentElement;
         modalHide(modal);
     })
     // После заполенения формы открываем модальное окно с благодарностью, и закрываем его по клику крестика или через 5 секунд
     thanksModalBtn.addEventListener('click', (e)=>{
-        showThanksModal(e);
+        e.preventDefault();
+        const form = document.querySelector('#call-back-form');
+
+        // Create an instance of Validator for the container element
+        const v = new Validator(form, {
+            onFieldChangeValidation: true,
+            lang: en,
+        });
+        v.validate();
+        if(v.validatorError.errorsList.length === 0){
+            form.reset();
+            showThanksModal();
+        }
     })
-    document.querySelector('#modal-thanks').querySelector('.modal__cross').addEventListener('click', (e)=>{
+    document.querySelector('#modal-thanks .modal__cross').addEventListener('click', (e)=>{
         const modal = document.querySelector('#modal-thanks');
         modalHide(modal);
         clearTimeout(modalThanksTimeout);
     })
     modalCallBackBtn.addEventListener('click', (e)=>{
-        modalHide(document.querySelector("#modal-call-back"));
-        showThanksModal(e);
+        e.preventDefault();
+        const modal = document.querySelector("#modal-call-back"),
+              form = modal.querySelector(".form")
+        // валидация формы
+        const v = new Validator(form, {
+            onFieldChangeValidation: true,
+            lang: en,
+        });
+        v.validate();
+        if(v.validatorError.errorsList.length === 0){
+            showThanksModal();
+            modalHide(modal);
+            form.reset();
+        }
     })
-
     // При нажатии по кнопке купить открываем модальное окно покупки и передаем в него название продукта который нужно купить
     document.addEventListener('click', (e)=>{
         if(e.target && (e.target.getAttribute('data-btn') === "btn-buy")){
             const productName = e.target.parentElement.parentElement.querySelector(".card__info").innerHTML,
-                  modal = document.querySelector("#modal-buy")
+                  modal = document.querySelector("#modal-buy");
             modal.querySelector(".modal__sub-title").innerHTML = productName;
             showModal(modal);
         }
     })
     // навешиваем обработчик событий на крестик который закрывает модальное окно
-    document.querySelector("#modal-buy").querySelector(".modal__cross").addEventListener('click', ()=>{
+    document.querySelector("#modal-buy .modal__cross").addEventListener('click', ()=>{
         modalHide(document.querySelector("#modal-buy"));
     })
-    // после нажатия кнопки купить в модальном окне закрываем модальное окно и открываем новое модальное окно с благодарностью
-    document.querySelector("#modal-buy").querySelector(".button_submit").addEventListener('click', (e)=>{
-        modalHide(document.querySelector("#modal-buy"));
-        showThanksModal(e)
+    // после нажатия кнопки купить и валидации формы в модальном окне закрываем модальное окно и открываем новое модальное окно с благодарностью
+    document.querySelector("#modal-buy .button_submit").addEventListener('click', (e)=>{
+        e.preventDefault();
+        const form = document.querySelector("#modal-buy .form")
+        const v = new Validator(form, {
+            onFieldChangeValidation: true,
+            lang: en,
+        });
+        v.validate();
+        if(v.validatorError.errorsList.length === 0){
+            modalHide(document.querySelector("#modal-buy"));
+            showThanksModal();
+            form.reset();
+        }
     })
 }
 export default  modal;
