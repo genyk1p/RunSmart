75c7e1212d37aecfba02274b5dd021808c0b333d
Evhen <geny.k1p@gmail.com>
2023-06-15 16:51:18 +0300

    aded scroll-up module

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,11 @@
 import slider from "./modules/slider";
 import cards from "./modules/cards";
 import modal from "./modules/modal"
+import crollUp from "./modules/scroll"
 
 document.addEventListener("DOMContentLoaded", () =>{
   slider();
   cards();
   modal();
+  crollUp();
 });
